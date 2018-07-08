fabf6b07fb45fd49e8b3a254407d8436ae27052f
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-08 14:20:55 -0500

    some footer stuff

--- src/footer/index.js
+++ src/footer/index.js
@@ -9,7 +9,7 @@ const Footer = ({}) => {
 					<li><a href="mailto:ammar.karachiwala@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
 				</ul>
 				<ul class="copyright">
-					<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					<li>&copy; Ammar Karachiwala</li><li><a href="ammarkarachi.com">ammarkarachi.com</a></li>
 				</ul>
 			</div>    
         </footer>
