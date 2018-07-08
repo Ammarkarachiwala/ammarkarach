034283053644a4e3a595ff622961d7b8ff218310
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-08 15:29:05 -0500

    updated the css and div date

---
+++ src/experience/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Experience = () => {
+    return <section >
+        <header class='major' >
+            <h2>Experience</h2>
+            <div >
+                <div className='date' >
+                    <h3>Senior Software Engineer </h3>
+                    <span>Oct 2016 - present</span>
+                </div>
+            </div>
+        </header>
+    </section>
+}
+
+export default Experience;
--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AboutMe from '../about-me';
-
+import Experience from '../experience';
 const Main = () => {
     return <div id='main' >
         <AboutMe />
+        <Experience />
 </div>
 }
 
