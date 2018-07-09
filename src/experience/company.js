18c8131a35d5cab068750fe69131b17b5cd02e69
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-08 21:36:21 -0500

    added alt

--- src/experience/company.js
+++ src/experience/company.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Company = ({image, name}) => {
     return <div className='company' > 
         <h3 >{name}</h3>
-        <img className='companyLogo' src={image} />
+        <img className='companyLogo' src={image} alt="" />
     </div>
 }
 export default Company;
