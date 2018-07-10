2b3e10d868bd3a3be25e74a9bc43012525ade653
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 21:36:37 -0500

    resume button

--- src/resume-button/index.js
+++ src/resume-button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ResumeButton = () => {
     return <div style={{ margin: '3em 0' }} > 
         <ul class="actions">
-	    	<li><a href="" class="button">Full Portfolio</a></li>
+	    	<li><a href="" class="button">Resume</a></li>
 	    	<div></div>
         </ul>
     </div>
