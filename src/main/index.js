6783faf965b91f9e4b3b6e33a69d2b298f475655
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 21:29:42 -0500

    added resume button

--- src/main/index.js
+++ src/main/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import AboutMe from '../about-me';
 import Experience from '../experience';
 import Education from '../education';
+import ResumeButton from '../resume-button';
 const Main = () => {
     return <div id='main' >
         <AboutMe />
         <Experience />
         <Education />
+        <ResumeButton />
 </div>
 }
 
---
+++ src/resume-button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResumeButton = () => {
+    return <div style={{ margin: '3em 0' }} > 
+        <ul class="actions">
+	    	<li><a href="" class="button">Full Portfolio</a></li>
+	    	<div></div>
+        </ul>
+    </div>
+}
+
+export default ResumeButton;
