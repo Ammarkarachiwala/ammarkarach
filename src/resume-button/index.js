6cbd9d52bf3e3e8890bb10f3fb82358525381c45
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 21:40:53 -0500

    added resume link

--- src/resume-button/index.js
+++ src/resume-button/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ResumeButton = () => {
     return <div style={{ margin: '3em 0' }} > 
         <ul class="actions">
-	    	<li><a href="" class="button">Resume</a></li>
+	    	<li><a  target='_blank' href="https://drive.google.com/file/d/0B-QbAKXtZDo3UU1UTWpWS3pVSHFRb0VVMmVKV1pkaE9Wc2dB/view?usp=sharing" class="button">Resume</a></li>
 	    	<div></div>
         </ul>
     </div>
