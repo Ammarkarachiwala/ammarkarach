926efced86a6939dca9b4ffdc7c72ab06e1ebad6
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 21:49:04 -0500

    feel free to contact me at

---
+++ src/contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return <section>
+        <header class='major' >
+            <h2>Contact</h2>
+
+        </header>
+    </section>
+}
+
+export default Contact;
--- src/main/index.js
+++ src/main/index.js
@@ -3,12 +3,15 @@ import AboutMe from '../about-me';
 import Experience from '../experience';
 import Education from '../education';
 import ResumeButton from '../resume-button';
+import Contact from '../contact';
+
 const Main = () => {
     return <div id='main' >
         <AboutMe />
         <Experience />
         <Education />
         <ResumeButton />
+        <Contact />
 </div>
 }
 
