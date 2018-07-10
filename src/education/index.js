f5bdba575424c723027256bf4553220d913c35f7
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 21:21:18 -0500

    education stuff

---
+++ src/education/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import UH from '../images/uh.png';
+import MU from '../images/mu.png';
+import Company from '../experience/company';
+import Position from '../experience/position';
+
+const Education = () => {
+    return <section>
+            <header class='major' >
+                <h2>Education</h2>
+                <div>
+                    <Company image={UH} name={'University of Houston'} />
+                    <Position
+                        position={"Master's Degree Computer Science"}
+                        date={'2009 - 2011'} 
+                     />
+                </div>
+                <hr class="minor" />
+                <div>
+                    <Company image={MU} name={'Mumbai University'} />
+                    <Position
+                        position={"Bacherlor of Engineering, Computer Engineering"}
+                        date={'2005 - 2009'} 
+                     />
+                </div>
+            </header>
+        </section>
+}
+
+export default Education;
--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutMe from '../about-me';
 import Experience from '../experience';
+import Education from '../education';
 const Main = () => {
     return <div id='main' >
         <AboutMe />
         <Experience />
+        <Education />
 </div>
 }
 
