11c436baf443a20a2bd72b839563ad41ba477131
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-09 19:23:19 -0500

    changed the logo

--- src/footer/index.js
+++ src/footer/index.js
@@ -6,10 +6,10 @@ const Footer = () => {
 				<ul class="icons">
 					<li><a href="https://www.linkedin.com/in/ammar-karachiwala-7b0b2215a/" class="icon fa-linkedin"><span class="label">Twitter</span></a></li>
 					<li><a href="https://github.com/Ammarkarachiwala" class="icon fa-github"><span class="label">Github</span></a></li>
-					<li><a href="mailto:ammar.karachiwala@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					<li><a href="mailto:me@ammarkarachi.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
 				</ul>
 				<ul class="copyright">
-					<li>&copy; Ammar Karachiwala</li><li><a href="ammarkarachi.com">ammarkarachi.com</a></li>
+					<li>&copy; Ammar Karachiwala</li><li><a href="/">ammarkarachi.com</a></li>
 				</ul>
 			</div>    
         </footer>
